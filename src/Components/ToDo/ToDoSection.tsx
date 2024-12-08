4f0799d686821b37762e1c60f6f58ae934e0002c
EngHussam23 <engraggad60@gmail.com>
2024-12-08 18:35:47 +0300

    Edit: some css

--- src/Components/ToDo/ToDoSection.tsx
+++ src/Components/ToDo/ToDoSection.tsx
@@ -1,3 +1,4 @@
+import AngleRight from "../../assets/Icons/AngleRight";
 import Search from "../../assets/Icons/Search";
 import Star from "../../assets/Icons/Star";
 
@@ -19,22 +20,26 @@ const ToDoSection = () => {
       </section>
       <section className="flex flex-col h-full w-full items-start gap-6">
         <h1 className="font-semibold text-4xl">My ToDo</h1>
-        <section className="flex flex-col h-full w-full gap-3">
-          <h3 className="font-medium text-2xl">My Day</h3>
+        <section className="flex flex-col w-full gap-3">
+          <h3 className="font-medium text-2xl p-2">My Day</h3>
           <div className="flex flex-col gap-4">
-            <div className="flex items-center justify-between p-4">
+            <div className="flex items-center justify-between p-4 hover:-bg--Neutral-100">
               <p>Task 1</p>
-              <Star color="#C9D3D8" />
+              <Star color="#4F5F6B" size={24} />
             </div>
-            <div className="flex items-center justify-between p-4">
+            <div className="flex items-center justify-between p-4 hover:-bg--Neutral-100">
               <p>Task 1</p>
-              <Star color="#C9D3D8" />
+              <Star color="#C9D3D8" size={24} />
             </div>
-            <div className="flex items-center justify-between p-4">
+            <div className="flex items-center justify-between p-4 hover:-bg--Neutral-100">
               <p>Task 1</p>
-              <Star color="#C9D3D8" />
+              <Star color="#C9D3D8" size={24} />
             </div>
           </div>
+        </section>
+        <section className="flex w-full items-center -text--Neutral-400 hover:-text--Primary-950 hover:cursor-pointer p-2 -bg--Neutral-200 rounded-lg">
+          <AngleRight color="#778C99" size={24} />
+          <h3 className="font-medium text-2xl">Completed Tasks</h3>
         </section>
       </section>
     </div>
